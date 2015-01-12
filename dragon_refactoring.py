f220dec51e13480b3bdad05366e4654bc91d5cd5
ArthurTerminus <ArthurTerminus@gmail.com>
2015-01-12 18:49:08 +0900

    Version26 うさぎを加える。

--- dragon_refactoring.py
+++ dragon_refactoring.py
@@ -485,12 +485,12 @@ class WaspModel(OpenGLModel):
 
         return WaspView
 
-
     def default_window_title(self):
         """スズメバチのウィンドウのタイトル(ラベル)を応答する。"""
         if TRACE: print __name__, self.default_window_title.__doc__
 
         return "Wasp"
+
 
 class WaspView(OpenGLView):
     """スズメバチのビュー。"""
@@ -501,19 +501,97 @@ class WaspView(OpenGLView):
 
         scaled_by_n = (lambda vertex: map((lambda value: value * 4.0), vertex))
         glBegin(GL_LINES)
-        glColor([ 1.0, 0.0, 0.0, 1.0 ])
-        glVertex(scaled_by_n([-1.00, 0.0, 0.0 ]))
-        glVertex(scaled_by_n([ 1.68, 0.0, 0.0 ]))
-        glColor([ 0.0, 1.0, 0.0, 1.0 ])
-        glVertex(scaled_by_n([ 0.0,-1.00, 0.0 ]))
-        glVertex(scaled_by_n([ 0.0, 1.68, 0.0 ]))
-        glColor([ 0.0, 0.0, 1.0, 1.0 ])
-        glVertex(scaled_by_n([ 0.0, 0.0,-1.00 ]))
-        glVertex(scaled_by_n([ 0.0, 0.0, 1.68 ]))
+        glColor([1.0, 0.0, 0.0, 1.0])
+        glVertex(scaled_by_n([-1.00, 0.0, 0.0]))
+        glVertex(scaled_by_n([1.68, 0.0, 0.0]))
+        glColor([0.0, 1.0, 0.0, 1.0])
+        glVertex(scaled_by_n([0.0, -1.00, 0.0]))
+        glVertex(scaled_by_n([0.0, 1.68, 0.0]))
+        glColor([0.0, 0.0, 1.0, 1.0])
+        glVertex(scaled_by_n([0.0, 0.0, -1.00]))
+        glVertex(scaled_by_n([0.0, 0.0, 1.68]))
         glEnd()
 
         return
 
+
+class BunnyModel(OpenGLModel):
+    """うさぎのモデル。"""
+
+    def __init__(self):
+        """うさぎのモデルのコンストラクタ。"""
+        if TRACE: print __name__, self.__init__.__doc__
+
+        super(BunnyModel, self).__init__()
+
+        filename = os.path.join(os.getcwd(), 'bunny.ply')
+        if os.path.exists(filename) and os.path.isfile(filename):
+            pass
+        else:
+            url = 'http://www.cc.kyoto-su.ac.jp/~atsushi/Programs/Bunny/bunny.ply'
+            urllib.urlretrieve(url, filename)
+
+        with open(filename, "rU") as a_file:
+            while True:
+                a_string = a_file.readline()
+                if len(a_string) == 0: break
+                a_list = a_string.split()
+                if len(a_list) == 0: continue
+                first_string = a_list[0]
+                if first_string == "element":
+                    second_string = a_list[1]
+                    if second_string == "vertex":
+                        number_of_vertexes = int(a_list[2])
+                    if second_string == "face":
+                        number_of_faces = int(a_list[2])
+                if first_string == "end_header":
+                    get_tokens = (lambda file: file.readline().split())
+                    collection_of_vertexes = []
+                    for n_th in range(number_of_vertexes):
+                        a_list = get_tokens(a_file)
+                        a_vertex = map(float, a_list[0:3])
+                        collection_of_vertexes.append(a_vertex)
+                    index_to_vertex = (lambda index: collection_of_vertexes[index])
+                    for n_th in range(number_of_faces):
+                        a_list = get_tokens(a_file)
+                        indexes = map(int, a_list[1:4])
+                        vertexes = map(index_to_vertex, indexes)
+                        a_tringle = OpenGLTriangle(*vertexes)
+                        self._display_object.append(a_tringle)
+                if first_string == "comment":
+                    second_string = a_list[1]
+                    if second_string == "eye_point_xyz":
+                        self._eye_point = map(float, a_list[2:5])
+                    if second_string == "sight_point_xyz":
+                        self._sight_point = map(float, a_list[2:5])
+                    if second_string == "up_vector_xyz":
+                        self._up_vector = map(float, a_list[2:5])
+                    if second_string == "zoom_height" and a_list[3] == "fovy":
+                        self._fovy = self._default_fovy = float(a_list[4])
+
+        return
+
+    def default_view_class(self):
+        """うさぎのモデルを表示するデフォルトのビューのクラスを応答する。"""
+        if TRACE: print __name__, self.default_view_class.__doc__
+
+        return BunnyView
+
+    def default_window_title(self):
+        """うさぎのウィンドウのタイトル(ラベル)を応答する。"""
+        if TRACE: print __name__, self.default_window_title.__doc__
+
+        return "Stanford Bunny"
+
+
+class BunnyView(OpenGLView):
+    """うさぎのビュー。"""
+
+    def display_axes(self):
+        """世界座標系を描画しない。"""
+        if TRACE: print __name__, self.display_axes.__doc__
+
+        return
 
 
 def main():
@@ -524,6 +602,9 @@ def main():
     a_model.open()
 
     a_model = WaspModel()
+    a_model.open()
+
+    a_model = BunnyModel()
     a_model.open()
 
     glutMainLoop()
